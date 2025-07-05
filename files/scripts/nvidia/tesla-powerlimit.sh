91bef9fcc1aab8fafa8aa3e88fc367e33ab887d9
CarrionAnimus <189206369+CarrionAnimus@users.noreply.github.com>
2025-07-05 18:39:55 -0500

    feat: init

---
+++ files/scripts/nvidia/tesla-powerlimit.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'Enabling Powerlimits for NVIDIA'
+
+# Service
+echo '[Unit]
+Description=NVIDIA Settings Configuration
+After=multi-user.target
+
+[Service]
+Type=oneshot
+ExecStartPre=/bin/sleep 15
+ExecStart=/usr/libexec/nvidia-powerlimit.sh
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/nvidia-powerlimit.service
+
+# Powerlimit
+echo '#!/usr/bin/env bash
+
+nvidia-smi -pm 1
+nvidia-smi -i 0 -pl 180' > /usr/libexec/nvidia-powerlimit.sh
+
+# Make executable
+chmod +x /usr/libexec/nvidia-powerlimit.sh
+
+systemctl enable nvidia-powerlimit.service
---
+++ files/system/etc/profile.d/fastfetch.sh
@@ -0,0 +1,4 @@
+alias neofetch='/usr/bin/fastfetch -c /usr/share/ublue-os/custom/fastfetch.jsonc'
+alias neowofetch='/usr/bin/fastfetch -c /usr/share/ublue-os/custom/fastfetch.jsonc'
+alias hyfetch='/usr/bin/fastfetch -c /usr/share/ublue-os/custom/fastfetch.jsonc'
+alias fastfetch='/usr/bin/fastfetch -c /usr/share/ublue-os/custom/fastfetch.jsonc'
